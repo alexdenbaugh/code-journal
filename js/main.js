ce381654622bb1539a116961fa58b85ce4fea0eb
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-03-31 11:40:44 -0700

    Use a loop to create a DOM tree for each journal entry in the data model and append it to the page when the 'DOMContentLoaded' event is fired. #2

--- js/main.js
+++ js/main.js
@@ -23,30 +23,37 @@ $formEnterNew.addEventListener('submit', function () {
   $formEnterNew.reset();
 });
 
-// function addEntry(entry) {
-//   var $li = document.createElement('li');
-//   $li.classList.add('row');
-//   var $divImg = document.createElement('div');
-//   $divImg.classList.add('column-half', 'new-image');
-//   var $img = document.createElement('img');
-//   $img.setAttribute('src', entry.url);
-//   $img.setAttribute('alt', entry.title);
-//   $divImg.appendChild($img);
-//   $li.appendChild($divImg)
-//   var $divTitleAndNotes = document.createElement('div');
-//   $divTitleAndNotes.classList.add('column-half');
-//   var $divTitle = document.createElement('div');
-//   var $h2Title = document.createElement('h2');
-//   $h2Title.classList.add('font');
-//   $h2Title.textContent = entry.title;
-//   $divTitle.appendChild($h2Title)
-//   $divTitleAndNotes.appendChild($divTitle)
-//   var $divNotes = document.createElement('div');
-//   var $pNotes = document.createElement('p');
-//   $pNotes.classList.add('font');
-//   $pNotes.textContent = entry.notes;
-//   $divNotes.appendChild($pNotes);
-//   $divTitleAndNotes.appendChild($divNotes)
-//   $li.appendChild($divTitleAndNotes)
-//   return $li;
-// }
+function addEntry(entry) {
+  var $li = document.createElement('li');
+  $li.classList.add('row');
+  var $divImg = document.createElement('div');
+  $divImg.classList.add('column-half', 'new-image');
+  var $img = document.createElement('img');
+  $img.setAttribute('src', entry.url);
+  $img.setAttribute('alt', entry.title);
+  $divImg.appendChild($img);
+  $li.appendChild($divImg);
+  var $divTitleAndNotes = document.createElement('div');
+  $divTitleAndNotes.classList.add('column-half');
+  var $divTitle = document.createElement('div');
+  var $h2Title = document.createElement('h2');
+  $h2Title.classList.add('font');
+  $h2Title.textContent = entry.title;
+  $divTitle.appendChild($h2Title);
+  $divTitleAndNotes.appendChild($divTitle);
+  var $divNotes = document.createElement('div');
+  var $pNotes = document.createElement('p');
+  $pNotes.classList.add('font');
+  $pNotes.textContent = entry.notes;
+  $divNotes.appendChild($pNotes);
+  $divTitleAndNotes.appendChild($divNotes);
+  $li.appendChild($divTitleAndNotes);
+  return $li;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var $ulElement = document.querySelector('ul');
+  for (var i = 0; i < data.entries.length; i++) {
+    $ulElement.appendChild(addEntry(data.entries[i]));
+  }
+});
