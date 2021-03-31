ba51535e892226f376a65a334c0590e0d456eb22
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-03-30 17:25:54 -0700

    Removed data-view guards and attributes. #1

--- js/main.js
+++ js/main.js
@@ -6,18 +6,12 @@ var $newImgURL = $formEnterNew.querySelector('img');
 
 $formEnterNew.addEventListener('input', function () {
   event.preventDefault();
-  if (event.target.getAttribute('data-view') !== 'newURL') {
-    return;
-  }
   var $inputURL = $formEnterNew.elements.url.value;
   $newImgURL.setAttribute('src', $inputURL);
 });
 
 $formEnterNew.addEventListener('submit', function () {
   event.preventDefault();
-  if (event.submitter.getAttribute('data-view') !== 'saveEntry') {
-    return;
-  }
   var formObject = {};
   formObject.title = $formEnterNew.elements.title.value;
   formObject.url = $formEnterNew.elements.url.value;
