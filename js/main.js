6bf5fedfab71e4cf95c58cbca2d16fca32c1b13e
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-04-01 17:32:27 -0700

    Add a click target for deleting an entry to the entry form that only appears if the user is editing an entry. #4

--- js/main.js
+++ js/main.js
@@ -109,21 +109,24 @@ document.addEventListener('click', function (event) {
     $newImgURL.setAttribute('src', 'images/placeholder-image-square.jpg');
     $formEnterNew.reset();
     $h1NewAndEditEntry.textContent = 'New Entry';
+    $deleteButton.classList.add('hidden');
   }
 });
 
 var $liTarget;
+var $deleteButton = document.querySelector('.delete-button');
 
 $ul.addEventListener('click', function (event) {
   if (event.target.nodeName !== 'I') {
     return;
   }
   changeView('entry-form');
+  $deleteButton.classList.remove('hidden');
   $h1NewAndEditEntry.textContent = 'Edit Entry';
   $liTarget = event.target.closest('li');
-  var $liEntryID = $liTarget.getAttribute('data-entry-id');
+  var liEntryID = $liTarget.getAttribute('data-entry-id');
   for (var i = 0; i < data.entries.length; i++) {
-    if (Number($liEntryID) === data.entries[i].entryId) {
+    if (Number(liEntryID) === data.entries[i].entryId) {
       data.editing = data.entries[i];
     }
   }
